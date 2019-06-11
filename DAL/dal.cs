9908b74550d7ded4c6a96d227c61cc81010a0958
Administrator <Administrator@HXT-20190514RQR>
2019-06-11 08:53:31 +0800

    dal的改动和webconfig的配置

--- DAL/dal.cs
+++ DAL/dal.cs
@@ -13,7 +13,7 @@ namespace DAL
 {
     public class dal
     {
-        public static SqlConnection SqlConnection()
+        public static SqlConnection SqlConnection()//连接字符串
         {
             string sqlconnectionString = ConfigurationManager.ConnectionStrings["sqlconnectionString"].ConnectionString;
             var connection = new SqlConnection(sqlconnectionString);
@@ -24,11 +24,11 @@ namespace DAL
 
             return connection;
         }
-        public List<AllInfo> GetInvitation()
+        public List<AllInfo> GetInvitation()//获取帖子的信息
         {
             using (IDbConnection conn = SqlConnection())
             {
-                var data = conn.Query<AllInfo>("select * from Invitation").ToList();
+                var data = conn.Query<AllInfo>("select * from Invitation ").ToList();
                 return data;
             }
         }
