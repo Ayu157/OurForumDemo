3927b023d0775f8c4a76a7a33c92c7ac57a9c894
Administrator <Administrator@HXT-20190514RQR>
2019-06-13 11:09:25 +0800

    帖子详情

--- DAL/dal.cs
+++ DAL/dal.cs
@@ -8,6 +8,7 @@ using MODEL;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using System.Configuration;
+using Newtonsoft.Json;
 using Log;
 
 namespace DAL
@@ -24,5 +25,24 @@ namespace DAL
                     return data;
             }
         }
+        public string GetXZInvitation(string IguId)
+        {
+            try
+            {
+                Log.FileLogService.Instance.Info("查询帖子详细信息，Id="+ IguId + "");
+                using (IDbConnection conn = Commond.SqlConnection())
+                {
+                    var da = conn.Query<AllInfo>("  select *  from UserInfo a inner join Invitation b on a.User_GuId=b.Inv_UserGuId where b.Inv_GuId='"+IguId+"'");
+                    var data = JsonConvert.SerializeObject(da);
+                    return data;
+                }
+            }
+            catch (Exception)
+            {
+                Log.FileLogService.Instance.Error("查询帖子详细信息失败！Id=" + IguId + "");
+                throw;
+            }
+        }
+
     }
 }
